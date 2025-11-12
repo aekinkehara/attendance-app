f4545c0d14ed3848999aff7ab6a62e1265497af5
aekinkehara <aekinkehara@gmail.com>
2025-11-12 14:12:16 +0700

    creating user models

--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -30,4 +30,30 @@ class AuthServices {
       rethrow;
     }
   }
+
+  // register with email and password
+  Future<UserCredential> registerWithEmailAndPassword(String email, String password) async {
+    try {
+      return await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password
+      );
+    } catch (e) {
+      if (e is FirebaseAuthException) {
+        if (e.code == 'operation-not-allowed') {
+          throw 'Email/password sign up is not enabled. Please contact support.';
+        }
+      }
+      rethrow;
+    }
+  }
+
+  // sign out
+  Future<void> signOut() async {
+    try {
+      await _auth.signOut();
+    } catch (e) {
+      rethrow;
+    }
+  }
 }
